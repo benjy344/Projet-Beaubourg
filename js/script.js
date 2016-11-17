086878a5baec3f59d16684a9b043a5f19c04c7b3
Benjamin DEMAIZIERE <demaiziere.benjamin@gmail.com>
2016-11-17 09:09:26 +0100

    mise en place de la popup pour code mirror.. a ameliorer

--- js/script.js
+++ js/script.js
@@ -154,7 +154,16 @@ function hidePopup() {
 
 
 
+$('.button').click(function(){
+  var buttonId = $(this).attr('id');
+  $('#modal-container').removeAttr('class').addClass(buttonId);
+  $('body').addClass('modal-active');
+})
 
+$('#modal-container').click(function(){
+  $(this).addClass('out');
+  $('body').removeClass('modal-active');
+});
 
 $(document).ready(function() {
     $('.hamburger').hide();
