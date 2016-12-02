d38f1121a71ead2ba69fdf88f38d4af7243d2170
Benjamin DEMAIZIERE <demaiziere.benjamin@gmail.com>
2016-12-02 20:44:45 +0100

    MAJ grunt + popin aide in prigress

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,7 +41,7 @@ module.exports = function(grunt) {
         'app/assets/sprites/*.png'
         ],
         dest: 'dist/assets/sprite-main.png',
-        destCss: 'app/css/scss/sprite-main.scss',
+        destCss: 'app/css/scss/utilities/_sprite-main.scss',
         cssFormat: 'scss',
         cssVarMap: function (sprite) {
          sprite.name = 'icon-' + sprite.name;
@@ -50,9 +50,47 @@ module.exports = function(grunt) {
        padding     : 5
      }
    },
-    // Observons…
-      watch: {
+    imagemin: {                          // Task 
+      png: {
         options: {
+          progressive: true,
+          optimizationLevel: 7
+        },
+        files: [
+          {
+            // Set to true to enable the following options…
+            expand: true,
+            // cwd is 'current working directory'
+            cwd: 'app/assets/',
+            src: ['*.png'],
+            // Could also match cwd line above. i.e. project-directory/img/
+            dest: 'dist/assets/',
+            ext: '.png'
+          }
+        ]
+      },
+      jpg: {
+        options: {
+          progressive: true,
+          optimizationLevel: 7
+        },
+        files: [
+          {
+            // Set to true to enable the following options…
+            expand: true,
+            // cwd is 'current working directory'
+            cwd: 'app/assets/',
+            src: ['*.jpg'],
+            // Could also match cwd. i.e. project-directory/img/
+            dest: 'dist/assets/',
+            ext: '.jpg'
+          }
+        ]
+      }
+    },
+    // Observons…
+    watch: {
+      options: {
           livereload: true, // Activons le livereload du navigateur
         },
         src: {
@@ -60,14 +98,18 @@ module.exports = function(grunt) {
           tasks: ['sass:dist', 'concat:dist', 'cssmin'], // … la commande à effectuer
         }
       }
-  })
+    })
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-spritesmith');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+
 
 
   grunt.registerTask('default', ['sass:dist', 'sprite', 'concat:dist', 'cssmin']);
   grunt.registerTask('spritesmith', ['sprite']);
+  grunt.registerTask('minifyImg', ['imagemin:png']);
+
 }
